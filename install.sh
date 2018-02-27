d5a41383afdc5bc780ca9b64a5f7c017dcfd52de
Ben Mills <bmills@adobe.com>
2018-02-27 08:57:07 -0700

    Use Python 2

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@
 
 if [ "$(uname -s)" == "Darwin" ]
 then
-  if [ `which python` != "/usr/local/bin/python" ];
+  if [ `which python2` != "/usr/local/bin/python2" ];
   then
     echo "  Installing Python for you."
     brew install python
-    PIP_REQUIRE_VIRTUALENV="" pip install --upgrade pip setuptools
-    PIP_REQUIRE_VIRTUALENV="" pip install virtualenv virtualenvwrapper psutil
+    PIP_REQUIRE_VIRTUALENV="" pip2 install --upgrade pip setuptools
+    PIP_REQUIRE_VIRTUALENV="" pip2 install virtualenv virtualenvwrapper psutil
   fi
 fi
 
