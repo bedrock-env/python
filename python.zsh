5524931686f57d4a7af44e89e19b12d8ee02b045
Ben Mills <ben@unfiniti.com>
2018-10-10 14:38:33 -0600

    Improve pathing

--- python.zsh
+++ python.zsh
@@ -1,13 +1,14 @@
 # Python
+PYTHON_BASE_DIR=/usr/local/opt/python
 export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export VIRTUALENVWRAPPER_PYTHON=$PYTHON_BASE_DIR/bin/python3
 export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 export PIP_RESPECT_VIRTUALENV=true
 if [[ -r /usr/local/bin/virtualenvwrapper.sh ]]; then
-    source /usr/local/bin/virtualenvwrapper.sh
+   source /usr/local/bin/virtualenvwrapper.sh
 else
-    echo "WARNING: Can't find virtualenvwrapper.sh"
+   echo "WARNING: Can't find virtualenvwrapper.sh"
 fi
 
 # pip should only run if there is a virtualenv currently activated
